b82e738f578da293d66d9e149e257908e9755914
ajrpTec <40459685+ajrp2008@users.noreply.github.com>
2021-12-05 01:34:58 +0100

    ...

--- HandIn3.java
+++ HandIn3.java
@@ -12,31 +12,33 @@ public class HandIn3 {
       public static void opret(StedTid a, StedTid b){
         liste.add(new Forbindelse(a,b));
       }
-      public String toString(){
-        return fra + "->" + til;
-      }
+      public String toString(){return fra + "->" + til;}
     }
 
-    static class StedTid implements Comparable<StedTid>{
-      public static ArrayList<StedTid> liste = new ArrayList<StedTid>();
+    static class StedTid{
+      public static int antalKnuder = 0;
+      public static ArrayList[] stKeyLister;
       int knudeNummer,stationsNummer,tidspunkt;
 
-      private StedTid(int s, int t, int k){
+      public StedTid(int s, int t, int k){
         knudeNummer    = k;
         stationsNummer = s;
         tidspunkt      = t;
       }
-      public int compareTo(StedTid stedTid) {
-        return stationsNummer - stedTid.stationsNummer;
+      public static void initOpslagStationStedTid(int antalAfgange){
+        stKeyLister = new ArrayList[antalAfgange];
+        for(int i = 0; i < stKeyLister.length ; i++)  stKeyLister[i] = new ArrayList<StedTid>();
       }
-      public String toString(){
-        return "( Station: " + stationsNummer + ", Tidspunkt : " + tidspunkt + " , Knude :" + knudeNummer + ")";
-      }
-      public static StedTid opret(int station, int tid, int knude){
-        StedTid st = new StedTid(station, tid, knude);
-        liste.add(st);
+      public static StedTid opret(int station, int tid, int knudeNr){
+        antalKnuder++;
+        StedTid st =  new StedTid(station,tid,knudeNr);
+        stKeyLister[station].add(st);
         return st;
       }
+      public static ArrayList<StedTid> hentSTer(int station){
+        return (ArrayList<StedTid>)stKeyLister[station];
+      }
+      public String toString(){return "( Station: "+stationsNummer+",tid: "+tidspunkt+",Knude :"+knudeNummer+")";}
     }
 
     public static void main(String[] args) {
@@ -46,6 +48,10 @@ public class HandIn3 {
               int antalAfgange    = in.readInt();
               int antalForspg     = in.readInt();
               int hjemmeStation   = in.readInt();
+
+              StedTid.initOpslagStationStedTid(antalAfgange);
+
+              Digraph totalForbindelsesGraf = new Digraph(antalStop);
 
               //Del A : Opret stedTid for hjemmestationen
               StedTid.opret(hjemmeStation,0,0);
@@ -57,42 +63,45 @@ public class HandIn3 {
                 int afgangsTid    = in.readInt();
                 int ankomstTid    = in.readInt(); // tid
 
+                totalForbindelsesGraf.addEdge(startStation,stopStation);
+
                 StedTid endeST = StedTid.opret(stopStation,ankomstTid,knudeNummer);
                 //Del C: Opret kant til denne stedTid fra alle stedTider hvor følgende gælder
-                //  hvis tidspunktet    <= afgangsTid
-                // hvis stationsNummer   = startStation
-                for(StedTid startST: StedTid.liste){
-                  if(startST.stationsNummer == startStation && startST.tidspunkt <= afgangsTid){
+                //  hvis tidspunktet    <= afgangsTid og hvis stationsNummer   = startStation
+                for(StedTid startST: StedTid.hentSTer(startStation)){
+                  if(startST.tidspunkt <= afgangsTid){
                       Forbindelse.opret(startST,endeST);
                   }
                 }
               }
 
-              Digraph G = new Digraph(StedTid.liste.size());
+              //for(StedTid st : StedTid.liste) StdOut.println(st);
+              //for(Forbindelse f : Forbindelse.liste) StdOut.println(f);
 
-              for(Forbindelse f : Forbindelse.liste)
-                G.addEdge(f.fra.knudeNummer,f.til.knudeNummer);
+              //Del D: Oprettelse af den rettede graf
+              Digraph dagsForbindelsesGraf       = new Digraph(StedTid.antalKnuder);
+              for(Forbindelse f : Forbindelse.liste) dagsForbindelsesGraf .addEdge(f.fra.knudeNummer,f.til.knudeNummer);
+              DirectedDFS dagforbindelserDFS = new DirectedDFS(dagsForbindelsesGraf ,0); //hardcoded at vi starter altid på 0
 
-              DirectedDFS dfs = new DirectedDFS(G,hjemmeStation);
+              DirectedDFS totalForbindelsesDFS = new DirectedDFS(totalForbindelsesGraf,hjemmeStation); //her brugers stationer som knuder
 
-              for(int forsprg = 0; forsprg < antalForspg ; forsprg++){
-                int inputStation  = in.readInt();
+              for(int i = 0 ; i < antalForspg ; i++){
+                int station = in.readInt();
 
-                if(dfs.marked(inputStation)){
-                  int tid = -1;
-                  for(StedTid st: StedTid.liste){
-                    if(st.stationsNummer ==  inputStation){
-                        if(tid == -1 || tid > st.tidspunkt) tid = st.tidspunkt;
-                    }
+                ArrayList<StedTid> ankomstListe = StedTid.hentSTer(station);
+
+                int hurtigsteTid = -1;
+                for(StedTid st : StedTid.hentSTer(station)){
+                  if(dagforbindelserDFS.marked(st.knudeNummer)){
+                    if(hurtigsteTid == -1 || hurtigsteTid > st.tidspunkt) hurtigsteTid = st.tidspunkt;
                   }
-                  StdOut.println(tid);
-                }else{
-                  StdOut.println("Umuligt!");
                 }
 
+                boolean erDerForbindelse = totalForbindelsesDFS.marked(station);
 
+                if(!erDerForbindelse)         StdOut.println("Jeg køber sku en bil");
+                else if( hurtigsteTid == -1)  StdOut.println("Ikke samme dag");
+                else                          StdOut.println(hurtigsteTid);
               }
-
     }
-
 }
